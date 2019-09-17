27173de7aa3948d0d670ffe13ab93439c3c60118
bimoprasetyoafif <bimoprasetyoafif@gmail.com>
2019-09-17 07:47:17 +0700

    Update models.py
    add model example to django app

--- exapmle_django/modelapp_example/models.py
+++ exapmle_django/modelapp_example/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CuriculumVitae(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=10)
+
+    def __str__ (self):
+        return self.name
