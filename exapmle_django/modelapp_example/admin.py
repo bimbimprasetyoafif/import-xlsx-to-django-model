bc608bf93d9f9e1b5ec3765335ed40ae70b6e248
bimoprasetyoafif <bimoprasetyoafif@gmail.com>
2019-09-17 08:00:50 +0700

    setting for import file

--- exapmle_django/modelapp_example/admin.py
+++ exapmle_django/modelapp_example/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import CuriculumVitae
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(CuriculumVitae)
+# admin.site.register(CuriculumVitae)
+@admin.register(CuriculumVitae)
+class ViewAdmin(ImportExportModelAdmin):
+    exclude = ('id',)
